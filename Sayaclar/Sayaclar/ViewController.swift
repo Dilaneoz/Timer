2dca1a54008acca04bf7da94898b015190be2091
Dilaneoz <dilansuozturk@gmail.com>
2023-09-15 21:47:41 +0300

    Timer

---
+++ Sayaclar/Sayaclar/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Sayaclar
+//
+//  Created by Dilan Öztürk on 13.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    var timer = Timer()
+    var kalanZaman = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       kalanZaman = 15
+        label.text = "Zaman: \(kalanZaman)"
+    }
+
+    @IBAction func baslatTiklandi(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFonksiyonu), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timerFonksiyonu(){
+        label.text = "Zaman: \(kalanZaman)"
+        kalanZaman = kalanZaman - 1
+        if kalanZaman == 0 {
+            label.text = "Süre Bitti"
+            timer.invalidate()
+            kalanZaman = 15
+        }
+    }
+    
+}
+
